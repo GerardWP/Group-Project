f4a1fe2af0238792263e8d6d38be29d7116102e9
Sam Warren <sjwarren91@gmail.com>
2019-08-01 21:12:33 +1000

    add button to play audio, finish text to speech API call and playback

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
+//function for converting a string to an mp3 byte array
 function textToSpeech(string) {
-
+    //creating the request body of the query
     data = {
         "input": {
             text: string
@@ -9,7 +10,7 @@ function textToSpeech(string) {
             "ssmlGender": "MALE"
         },
         "audioConfig": {
-            "audioEncoding": "MP3"
+            "audioEncoding": "MP3" 
         }
     }
 
@@ -20,11 +21,62 @@ function textToSpeech(string) {
         contentType: "application/json",
         data: JSON.stringify(data),
         processData: false,
-        success: function(response){
-            console.log(response);
+        success: function (response) {
+            console.log(atob(response.audioContent));
+            return test = Uint8Array.from(atob(response.audioContent), c => c.charCodeAt(0));
         }
     });
 }
 
+// var test;
+var context; // Audio context
+var buf; // Audio buffer
 
-textToSpeech("Hello World");
+function init() {
+    if (!window.AudioContext) {
+        if (!window.webkitAudioContext) {
+            alert("Your browser does not support any AudioContext and cannot play back this audio.");
+            return;
+        }
+        window.AudioContext = window.webkitAudioContext;
+    }
+    console.log('create audio context')
+    context = new AudioContext();
+}
+
+function playByteArray(byteArray) {
+
+    var arrayBuffer = new ArrayBuffer(byteArray.length);
+    var bufferView = new Uint8Array(arrayBuffer);
+    for (i = 0; i < byteArray.length; i++) {
+        bufferView[i] = byteArray[i];
+    }
+
+    console.log('before decode');
+
+    let x = context.decodeAudioData(arrayBuffer, function (buffer) {
+        buf = buffer;
+        console.log('after decode');
+        play();
+    }).catch(err => console.error(err));
+}
+
+// Play the loaded file
+function play() {
+    console.log('play');
+    // Create a source node from the buffer
+    var source = context.createBufferSource();
+    source.buffer = buf;
+    // Connect to the final output node (the speakers)
+    source.connect(context.destination);
+    // Play immediately
+    let x = source.start(0);
+}
+
+var test = textToSpeech("I hope you get food poisoning");
+
+$("#click").on("click", function () {
+    
+    init();
+    playByteArray(test);
+})
